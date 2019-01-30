0e0bcb3d9e2234bf2e6f73ab6a2d30f03e5f24d5
michalciurus <michael.ciurus@gmail.com>
2019-01-30 09:50:00 +0800

    fix formatting

--- Source/main.swift
+++ Source/main.swift
@@ -44,7 +44,7 @@ func findXibs(folder: Folder) {
 func parseXib(file: File) {
     let fileString = try! file.readAsString()
     let xml = SWXMLHash.parse(fileString)
-
+    
     readChildren(xml: xml)
 }
 
@@ -79,17 +79,17 @@ func readChildren(xml: XMLIndexer) {
             
             var existingCode = viewControllersExtensionCode[vc] ?? ""
             
-            existingCode = existingCode + "\n var \(restId): \(className)! {"
-            existingCode = existingCode + "\n get { return objc_getAssociatedObject(self, \(restId)Key.address) as? \(className) }"
-            existingCode = existingCode + "\n set { }"
-            existingCode = existingCode + "\n }"
+            existingCode = existingCode + "\n    var \(restId): \(className)! {"
+            existingCode = existingCode + "\n        get { return objc_getAssociatedObject(self, \(restId)Key.address) as? \(className) }"
+            existingCode = existingCode + "\n        set { }"
+            existingCode = existingCode + "\n    }"
             
             viewControllersExtensionCode[vc] = existingCode
             
-            keysCode = keysCode + "\n var \(restId)Key = AssociatedObjectStringKey(key: \"\(restId)\")"
+            keysCode = keysCode + "\nvar \(restId)Key = AssociatedObjectStringKey(key: \"\(restId)\")"
             
             if keysArrayCode != "" {
-                keysArrayCode = keysArrayCode + ","
+                keysArrayCode = keysArrayCode + ", "
             }
             
             keysArrayCode = keysArrayCode + "\(restId)Key"
@@ -101,115 +101,121 @@ func readChildren(xml: XMLIndexer) {
 
 findXibs(folder: folder)
 
+try! file.append(string: "//Auto Generated Code \n\n")
+
+
 try! file.append(string: "import UIKit")
 
 for module in modules {
-    try! file.append(string: "\n import \(module)")
+    try! file.append(string: "\nimport \(module)")
 }
+
+try! file.append(string: "\n \n")
 
 try! file.append(string: keysCode)
 
 for vcKey in viewControllersExtensionCode.keys {
-    try! file.append(string: "\n extension \(vcKey) {")
+    try! file.append(string: "\n \n")
+    try! file.append(string: "\nextension \(vcKey) {")
     try! file.append(string: viewControllersExtensionCode[vcKey]!)
     try! file.append(string: "\n }")
 }
 
-try! file.append(string: "\n let AllAssociatedObjectsKeys = ")
+try! file.append(string: "\n \n")
+
+try! file.append(string: "\nlet AllAssociatedObjectsKeys = ")
 try! file.append(string: "[")
 try! file.append(string: "\(keysArrayCode)")
 try! file.append(string: "]")
 
-
+try! file.append(string: "\n\n//Swizzling Code \n\n")
 
 let logicCode = """
 
 class AssociatedObjectStringKey {
-let key: String
-
-init(key: String) {
-self.key = key
-}
-
-var address: UnsafeRawPointer {
-return UnsafeRawPointer(bitPattern: abs(key.hashValue))!
-}
+    let key: String
+    
+    init(key: String) {
+        self.key = key
+    }
+    
+    var address: UnsafeRawPointer {
+        return UnsafeRawPointer(bitPattern: abs(key.hashValue))!
+    }
 }
 
 public protocol SwizzlingInjection: class {
-static func inject()
+    static func inject()
 }
 
 class SwizzlingHelper {
-
-private static let doOnce: Any? = {
-UIViewController.inject()
-return nil
-}()
-
-static func enableInjection() {
-_ = SwizzlingHelper.doOnce
-}
+    
+    private static let doOnce: Any? = {
+        UIViewController.inject()
+        return nil
+    }()
+    
+    static func enableInjection() {
+        _ = SwizzlingHelper.doOnce
+    }
 }
 
 extension UIApplication {
-
-override open var next: UIResponder? {
-SwizzlingHelper.enableInjection()
-return super.next
-}
-
+    
+    override open var next: UIResponder? {
+        SwizzlingHelper.enableInjection()
+        return super.next
+    }
+    
 }
 
 extension UIViewController: SwizzlingInjection
 {
-
-public static func inject() {
-let originalSelector = #selector(UIViewController.loadView)
-let swizzledSelector = #selector(UIViewController.myViewDidLoad)
-
-let originalMethod = class_getInstanceMethod(self, originalSelector)!
-let swizzledMethod = class_getInstanceMethod(self, swizzledSelector)!
-
-let didAddMethod = class_addMethod(self, originalSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod))
-
-if didAddMethod {
-class_replaceMethod(self, swizzledSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod))
-} else {
-method_exchangeImplementations(originalMethod, swizzledMethod);
-}
-
-}
-
-@objc func myViewDidLoad() {
-self.myViewDidLoad()
-
-findAllViewsWithRestoration(viewToInpect: view)
-}
-
-func findAllViewsWithRestoration(viewToInpect: UIView) {
-for view in viewToInpect.subviews {
-if view.restorationIdentifier != nil {
-
-var keyFound: AssociatedObjectStringKey? = nil
-
-AllAssociatedObjectsKeys.forEach { (key) in
-if key.key == view.restorationIdentifier {
-keyFound = key
-}
-}
-
-objc_setAssociatedObject(self, keyFound!.address, view, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
-}
-
-findAllViewsWithRestoration(viewToInpect: view)
-}
-}
+    
+    public static func inject() {
+        let originalSelector = #selector(UIViewController.loadView)
+        let swizzledSelector = #selector(UIViewController.myViewDidLoad)
+        
+        let originalMethod = class_getInstanceMethod(self, originalSelector)!
+        let swizzledMethod = class_getInstanceMethod(self, swizzledSelector)!
+        
+        let didAddMethod = class_addMethod(self, originalSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod))
+        
+        if didAddMethod {
+            class_replaceMethod(self, swizzledSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod))
+        } else {
+            method_exchangeImplementations(originalMethod, swizzledMethod);
+        }
+        
+    }
+    
+    @objc func myViewDidLoad() {
+        self.myViewDidLoad()
+        
+        findAllViewsWithRestoration(viewToInpect: view)
+    }
+    
+    func findAllViewsWithRestoration(viewToInpect: UIView) {
+        for view in viewToInpect.subviews {
+            if view.restorationIdentifier != nil {
+                
+                var keyFound: AssociatedObjectStringKey? = nil
+                
+                AllAssociatedObjectsKeys.forEach { (key) in
+                    if key.key == view.restorationIdentifier {
+                        keyFound = key
+                    }
+                }
+                
+                objc_setAssociatedObject(self, keyFound!.address, view, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            }
+            
+            findAllViewsWithRestoration(viewToInpect: view)
+        }
+    }
 }
 
 """
 
-
 try! file.append(string: logicCode)
-
 
